a01e854e4d988b55b5751121df591da7ae3a7052
aftab82389 <130346457+aftab82389@users.noreply.github.com>
2024-12-22 16:23:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,28 +20,6 @@ const countdown = setInterval(() => {
     celebrationSound.play();
   }
 }, 1000);
-const countdownElement = document.getElementById('countdown');
-const celebrationSound = document.getElementById('celebrationSound');
-
-const newYearDate = new Date('January 1, 2025 00:00:00').getTime();
-
-const countdown = setInterval(() => {
-  const now = new Date().getTime();
-  const distance = newYearDate - now;
-
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  countdownElement.innerHTML = `${days}  d  ${hours} h ${minutes} m ${seconds} s`;
-
-  if (distance < 0) {
-    clearInterval(countdown);
-    countdownElement.innerHTML = "Happy New Year!";
-    celebrationSound.play();
-  }
-}, 1000);
 const celebrate=document.querySelector('.celebrate');
 document.getElementById('celebrateBtn').addEventListener('click', () => {
   celebrate.style.display="block";
